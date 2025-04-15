b9a89f12544a257d7b07749d76b8c510c5f240a6
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-04-15 19:37:40 +0700

    File Array

---
+++ struct.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+};
+
+int main() {
+    //membuat object struct mahasiswa
+    mahasiswa mhs;
+
+    //mengakses struct mahasiswa
+    mhs.nim = "20220140000";
+    mhs.nama = "Abra";
+    mhs.alamat = "Kasihan";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan ALAMAT = ";
+    cin >> mhs.alamat;
+
+    cout << "NIM = " <<mhs.nim << endl;
+    cout << "NAMA = " <<mhs.nama << endl;
+    cout << "ALAMAT = " <<mhs.alamat << endl;
+
+}
