90ebe993b28e1cb08f6e5365508361a69dd3abb5
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-04-15 19:50:12 +0700

    Pengaksesan Struct Mahasiswa

--- struct.cpp
+++ struct.cpp
@@ -10,4 +10,21 @@ struct mahasiswa{
 int main() {
     //membuat object struct mahasiswa
     mahasiswa mhs;
+
+      //mengakses struct mahasiswa
+      mhs.nim = "20220140000";
+      mhs.nama = "Abra";
+      mhs.alamat = "Kasihan";
+  
+      cout << "Masukkan NIM = ";
+      cin >> mhs.nim;
+      cout << "Masukkan NAMA = ";
+      cin >> mhs.nama;
+      cout << "Masukkan ALAMAT = ";
+      cin >> mhs.alamat;
+  
+      cout << "NIM = " <<mhs.nim << endl;
+      cout << "NAMA = " <<mhs.nama << endl;
+      cout << "ALAMAT = " <<mhs.alamat << endl;
+  
 }
