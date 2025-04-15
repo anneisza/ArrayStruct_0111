e6331a6b4180bf900a9752c20b03e149ffbd856d
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-04-15 19:48:05 +0700

    Membuat File Struct

--- struct.cpp
+++ struct.cpp
@@ -6,25 +6,3 @@ struct mahasiswa{
     string nama;
     string alamat;
 };
-
-int main() {
-    //membuat object struct mahasiswa
-    mahasiswa mhs;
-
-    //mengakses struct mahasiswa
-    mhs.nim = "20220140000";
-    mhs.nama = "Abra";
-    mhs.alamat = "Kasihan";
-
-    cout << "Masukkan NIM = ";
-    cin >> mhs.nim;
-    cout << "Masukkan NAMA = ";
-    cin >> mhs.nama;
-    cout << "Masukkan ALAMAT = ";
-    cin >> mhs.alamat;
-
-    cout << "NIM = " <<mhs.nim << endl;
-    cout << "NAMA = " <<mhs.nama << endl;
-    cout << "ALAMAT = " <<mhs.alamat << endl;
-
-}
