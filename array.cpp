bf3415171f808b1571cc5c8b2d2a9b0ac6575897
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-04-15 19:44:38 +0700

    Membuat Perulangan For

--- array.cpp
+++ array.cpp
@@ -15,6 +15,16 @@ int main() {
     skor[2] = 100;
     //menampilkan isi array pada index tertentu
     cout << "Tampilkan skor ke 3 = " << skor[2] << endl;
+
+    //mengisi array dengan loop for
+    for (int n = 0; n < 3; n++)
+    {
+        cout << "Mahasiswa ke-" << n + 1 << endl;
+        cout << "Nama Mahasiswa = ";
+        cin >> nama[n];
+    }
+  
+
     
 }
 
