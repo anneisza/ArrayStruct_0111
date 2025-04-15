c0b893374ebe88855e85910279c92a2d958acb10
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-04-15 19:40:08 +0700

    Deklarasi Array

---
+++ array.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi array tenapa ukuran, wajib diisi data
+int nilai []={1,2,3};
+//deklarasi array dengan ukuran, didisi data awal
+int skor [4]={12,24,48,64};
+//deklarasi array dengan ukuran, tanpa nilai awal
+string nama[3];
