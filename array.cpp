062452494325be199c4c7253512fd77ee492b95f
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-04-15 19:42:04 +0700

    Membuat Program Utama

--- array.cpp
+++ array.cpp
@@ -7,3 +7,14 @@ int nilai []={1,2,3};
 int skor [4]={12,24,48,64};
 //deklarasi array dengan ukuran, tanpa nilai awal
 string nama[3];
+
+int main() {
+    //menampilkan isi array pada index tertentu
+    cout << "Tampilkan skor nilai ke 3 = " << skor [2]<< endl;
+    //mengisi array pada index tertentu
+    skor[2] = 100;
+    //menampilkan isi array pada index tertentu
+    cout << "Tampilkan skor ke 3 = " << skor[2] << endl;
+    
+}
+
