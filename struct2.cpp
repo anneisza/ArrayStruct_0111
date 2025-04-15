ce2f2513911528a21351b4a9f87b9dbabe15eca8
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-04-15 19:53:01 +0700

    Membuat Program Utama Struct 2

--- struct2.cpp
+++ struct2.cpp
@@ -11,3 +11,26 @@ struct mahasiswa{
     string nama;
     detailAlamat alamat;
 };
+
+
+int main() {
+    //membuat object struct mahasiswa
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan ALAMAT DESA = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan ALAMAT KOTA = ";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+    cout << "Data Mahasiswa " << endl;
+
+    cout << "NIM = " <<mhs.nim << endl;
+    cout << "NAMA = " <<mhs.nama << endl;
+    cout << "ALAMAT DESA = " <<mhs.alamat.desa << endl;
+    cout << "ALAMAT KOTA = " <<mhs.alamat.kota << endl;
+}
